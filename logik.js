7017109233276402bbfb55061248af3b262968da
Niko <nikolerch@t-online.de>
2024-07-23 14:43:08 +0200

    Update 23.07 14:43

--- logik.js
+++ logik.js
@@ -1,3 +1,19 @@
 const date = new Date();
+const month = date.getMonth() + 1;
+const year = date.getYear() + 1900;
 
-console.log(date)
+
+console.log(date);
+
+
+let weekdays = [ "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+
+   let januar =  for ( let i = 1 ; i <=31 ; i++) 
+        console.log(i)
+
+
+
+
+
+
+
